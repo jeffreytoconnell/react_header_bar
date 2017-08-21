9d1abe30fb02b78f1678ebc58f17d7a69936ea84
jeffreytoconnell <jeffreytoconnell@gmail.com>
2017-08-21 16:07:05 -0700

    initail commit

---
+++ src/components/navigation-bar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './navigation-bar.css';
+
+export default function NavigationBar(props) {
+    const links = props.links.map((link, index) => (
+        <li key={index}>
+            <a href={link.href}>
+                {link.text}
+            </a>
+        </li>
+    ));
+
+    return (
+        <div className="navigation-bar">
+            <h1>{props.title}</h1>
+            <nav className="navigation-bar-nav">
+                <ul>
+                    {links}
+                </ul>
+            </nav>
+        </div>
+    );
+}
